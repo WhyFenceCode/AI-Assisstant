f9ed926b52977aaeaf9471ed7dee58fae1708928
WhyFenceCode <whyfencecode@gmail.com>
2023-04-05 15:50:18 -0700

    added weather command structure

--- mainloop.py
+++ mainloop.py
@@ -1,4 +1,11 @@
-
+import speech
+import time
+from generalCommands import search
+from specificCommands import weather
 
 def mainLoop():
-    
+    while True:
+        phrase = speech.input("")
+
+        if 'weather' && 'show' in phrase.split():
+            weather()
