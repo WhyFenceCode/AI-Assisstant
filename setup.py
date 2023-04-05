cbc7a54499c7bd7cba7b463a4287f893092d9c9d
WhyFenceCode <whyfencecode@gmail.com>
2023-04-05 12:10:43 -0700

    Started register function

--- setup.py
+++ setup.py
@@ -17,10 +17,22 @@ return setupArray
 
 def register():
 
+    emailreg = True
+    usernamereg = True
+    firstnamereg = True
+    agereg = True
+    pronounsreg = True
+    birthdayreg = True
+
+
+    with open('users.txt', 'a') as y:
+    y.write('\n')
+    y.write(emailreg + "|" + usernamereg + "|" + firstnamereg + "|" + agereg + "|" + pronounsreg + "|" + birthdayreg)
+    y.close
 
 def login():
     users = open("users.txt").read().split("|")
-    if users[0] != " ":
+    if users[0] != "":
         loginarryInternal[i] = users[i]
         return loginarryInternal
     else:
